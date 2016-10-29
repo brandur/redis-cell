dc79890f37f0251d391d275cd188c16debbf957a
Brandur <brandur@mutelight.org>
2016-10-29 15:12:01 -0700

    Add "throttle" command
    This is not yet functional.

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,8 @@ pub struct RedisModuleCtx;
 #[repr(C)]
 pub struct RedisModuleString;
 
+type RedisModuleCmdFunc = extern "C" fn(ctx: *mut RedisModuleCtx, argv: *mut RedisModuleString, argc: c_int) -> c_int;
+
 // Redis doesn't make this easy for us by exporting a library, so instead what
 // we do is bake redismodule.h's symbols into a library of our construction
 // during build and link against that. See build.rs for details.
@@ -27,6 +29,20 @@ extern {
     fn Export_RedisModule_Init(ctx: *mut RedisModuleCtx,
                                modulename: *const u8, module_version: c_int,
                                api_version: c_int) -> c_int;
+
+    fn RedisModule_CreateCommand(ctx: *mut RedisModuleCtx, name: *const u8,
+                                 cmdfunc: RedisModuleCmdFunc,
+                                 strflags: *const u8, firstkey: c_int,
+                                 lastkey: c_int, keystep: c_int) -> c_int;
+}
+
+#[allow(non_snake_case)]
+#[allow(unused_variables)]
+#[no_mangle]
+pub extern "C" fn Throttle_RedisCommand(ctx: *mut RedisModuleCtx,
+                                        argv: *mut RedisModuleString,
+                                        argc: c_int) -> c_int {
+    return REDISMODULE_OK;
 }
 
 #[allow(non_snake_case)]
@@ -40,6 +56,13 @@ pub extern "C" fn RedisModule_OnLoad(ctx: *mut RedisModuleCtx,
                                    format!("{}{}", MODULE_NAME, "\0").as_ptr(),
                                    MODULE_VERSION, REDISMODULE_APIVER_1)
                                    == REDISMODULE_ERR {
+            return REDISMODULE_ERR;
+        }
+
+        if RedisModule_CreateCommand(ctx, "throttle\0".as_ptr(),
+                                     Throttle_RedisCommand,
+                                     ("readonly\0").as_ptr(), 0, 0, 0)
+                                     == REDISMODULE_ERR {
             return REDISMODULE_ERR;
         }
     }
