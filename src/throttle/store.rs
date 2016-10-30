36ce2d770414fd8381f3a636e39a0c8cba522269
Brandur <brandur@mutelight.org>
2016-10-30 14:58:19 -0700

    Move implement around (no op)

--- src/throttle/store.rs
+++ src/throttle/store.rs
@@ -3,6 +3,16 @@ extern crate time;
 use std::error;
 use std::fmt;
 
+pub trait Store {
+    fn compare_and_swap_with_ttl(&self, key: &str, old: i64, new: i64,
+                                 ttl: time::Tm) -> Result<bool, StoreError>;
+
+    fn get_with_time(&self, key: &str) -> Result<(i64, time::Tm), StoreError>;
+
+    fn set_if_not_exists_with_ttl(&self, key: &str, value: i64,
+                                  ttl: time::Tm) -> Result<bool, StoreError>;
+}
+
 #[derive(Debug)]
 pub struct StoreError<'a> {
     message: &'a str,
@@ -28,14 +38,4 @@ impl<'a> error::Error for StoreError<'a> {
     fn cause(&self) -> Option<&error::Error> {
         None
     }
-}
-
-pub trait Store {
-    fn compare_and_swap_with_ttl(&self, key: &str, old: i64, new: i64,
-                                 ttl: time::Tm) -> Result<bool, StoreError>;
-
-    fn get_with_time(&self, key: &str) -> Result<(i64, time::Tm), StoreError>;
-
-    fn set_if_not_exists_with_ttl(&self, key: &str, value: i64,
-                                  ttl: time::Tm) -> Result<bool, StoreError>;
 }
