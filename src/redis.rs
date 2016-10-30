fbe6bb500ce8fd521f06de640618c261e73fa42c
Brandur <brandur@mutelight.org>
2016-10-30 11:35:05 -0700

    More functions from the Redis interface

--- src/redis.rs
+++ src/redis.rs
@@ -1,5 +1,5 @@
 extern crate libc;
-use libc::{c_int, c_longlong};
+use libc::{c_int, c_longlong, size_t};
 
 // Rust can't link against C macros (#define) so we just redefine them here.
 // There's a ~0 chance that any of these will ever change so it's pretty safe.
@@ -7,10 +7,13 @@ pub const REDISMODULE_APIVER_1: c_int = 1;
 pub const REDISMODULE_OK: c_int = 0;
 pub const REDISMODULE_ERR: c_int = 1;
 
-#[allow(improper_ctypes)]
 #[derive(Clone, Copy)]
 #[repr(C)]
 pub struct RedisModuleCtx;
+
+#[derive(Clone, Copy)]
+#[repr(C)]
+pub struct RedisModuleKey;
 
 #[derive(Clone, Copy)]
 #[repr(C)]
@@ -32,14 +35,25 @@ extern {
                                modulename: *const u8, module_version: c_int,
                                api_version: c_int) -> c_int;
 
+    pub static RedisModule_CloseKey:
+        extern "C" fn(kp: *mut RedisModuleKey);
+
     pub static RedisModule_CreateCommand:
         extern "C" fn(ctx: *mut RedisModuleCtx, name: *const u8,
                       cmdfunc: Option<RedisModuleCmdFunc>,
                       strflags: *const u8, firstkey: c_int,
                       lastkey: c_int, keystep: c_int) -> c_int;
 
+    pub static RedisModule_CreateString:
+        extern "C" fn(ctx: *mut RedisModuleCtx, ptr: *const u8, len: size_t)
+                      -> *mut RedisModuleString;
+
     pub static RedisModule_GetSelectedDb:
         extern "C" fn(ctx: *mut RedisModuleCtx) -> c_int;
+
+    pub static RedisModule_OpenKey:
+        extern "C" fn(ctx: *mut RedisModuleCtx,
+                      keyname: *mut RedisModuleString, mode: c_int);
 
     pub static RedisModule_ReplyWithLongLong:
         extern "C" fn(ctx: *mut RedisModuleCtx, ll: c_longlong)
@@ -48,4 +62,8 @@ extern {
     pub static RedisModule_ReplyWithString:
         extern "C" fn(ctx: *mut RedisModuleCtx, str: *mut RedisModuleString)
                        -> c_int;
+
+    pub static RedisModule_StringSet:
+        extern "C" fn(key: *mut RedisModuleKey, str: *mut RedisModuleString)
+                      -> c_int;
 }
