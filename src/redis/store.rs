7984b8a00e21314bb64822ed66705719e15230ae
Brandur <brandur@mutelight.org>
2016-10-30 15:19:55 -0700

    Start RedisStore implementation

--- src/redis/store.rs
+++ src/redis/store.rs
@@ -2,13 +2,24 @@ extern crate time;
 
 use throttle::store;
 
-struct RedisStore {
+pub struct RedisStore<'a> {
+    db: i64,
+    prefix: &'a str,
 }
 
-impl store::Store for RedisStore {
+impl<'a> RedisStore<'a> {
+    pub fn new(db: i64) -> RedisStore<'a> {
+        RedisStore{prefix: "", db: db}
+    }
+
+    pub fn new_with_prefix(prefix: &str, db: i64) -> RedisStore {
+        RedisStore{prefix: prefix, db: db}
+    }
+}
+
+impl<'a> store::Store for RedisStore<'a> {
     fn compare_and_swap_with_ttl(&self, key: &str, old: i64, new: i64,
                                  ttl: time::Tm) -> Result<bool, store::StoreError> {
-        Result::Err(store::StoreError::new("not implemented"))
     }
 
     fn get_with_time(&self, key: &str) -> Result<(i64, time::Tm), store::StoreError> {
