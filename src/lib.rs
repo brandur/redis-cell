251f0cbd998eb919db176ddcc5aa8468811c413a
Brandur <brandur@mutelight.org>
2016-11-02 17:04:59 -0700

    Much more succinct implementation

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,6 @@ use error::ThrottleError;
 use libc::c_int;
 use redis::raw::*;
 use redis::store::RedisStore;
-use std::error::Error;
 
 const MODULE_NAME: &'static str = "redis-throttle";
 const MODULE_VERSION: c_int = 1;
@@ -30,22 +29,24 @@ impl ThrottleCommand {
 impl redis::Command for ThrottleCommand {
     fn run(&self, r: redis::Redis, args: &[&str]) -> redis::CommandResult {
         if args.len() != 4 && args.len() != 5 {
-            return Err(ThrottleError::generic("throttle command expects either 4 or 5 arguments"));
+            return Err(ThrottleError::generic("Usage: throttle <bucket> <max_burst> \
+                                               <count> <period> [<quantity>]"));
         }
 
-        let bucket = args[0];
-        let max_burst =
-            try!(args[1].parse::<i64>().map_err(|e| ThrottleError::generic(e.description())));
-        let count =
-            try!(args[2].parse::<i64>().map_err(|e| ThrottleError::generic(e.description())));
-        let period =
-            try!(args[3].parse::<i64>().map_err(|e| ThrottleError::generic(e.description())));
-        let quantity = match args.get(4) {
-            Some(n) => {
-                let quantity = try!(n.parse::<i64>()
-                    .map_err(|e| ThrottleError::generic(e.description())));
-                quantity
-            }
+        let parse_i64 = |arg: &str| -> Result<i64, ThrottleError> {
+            arg.parse::<i64>()
+                .map_err(|_| {
+                    ThrottleError::generic(format!("Couldn't parse as integer: {}", arg).as_str())
+                })
+        };
+
+        // the first argument is command name "throttle" (ignore it)
+        let bucket = args[1];
+        let max_burst = try!(parse_i64(args[2]));
+        let count = try!(parse_i64(args[3]));
+        let period = try!(parse_i64(args[4]));
+        let quantity = match args.get(5) {
+            Some(n) => try!(parse_i64(n)),
             None => 1,
         };
 
