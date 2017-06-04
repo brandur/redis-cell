26b0cc418d5e4ef77b17746ed381d76151bd8bbe
Daniel Werner <dan.werner@gmail.com>
2017-06-04 09:46:13 -0700

    remove extraneous stringify!()

--- redis-module-sys/src/redis/macros.rs
+++ redis-module-sys/src/redis/macros.rs
@@ -33,7 +33,7 @@ macro_rules! redis_module {
                                              argc: libc::c_int)
                                              -> ::redis_module_sys::redis::raw::Status {
             if ::redis_module_sys::redis::raw::init(ctx,
-                         format!("{}\0", stringify!($name)).as_ptr(),
+                         format!("{}\0", $name).as_ptr(),
                          $ver,
                          ::redis_module_sys::redis::raw::REDISMODULE_APIVER_1) == ::redis_module_sys::redis::raw::Status::Err {
                 return ::redis_module_sys::redis::raw::Status::Err;
